c3582496de4a99f3e3a424696538b4d49e88e296
Nino Sopoian <ninosopoian@gmail.com>
2024-01-13 23:22:41 +0400

    add header and burger menu

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+function burger() {
+    const burgerMenu = document.querySelector('#burger-menu')
+
+    burgerMenu.addEventListener('click', ()=> {
+        burgerMenu.classList.toggle('active')
+        
+    })
+     
+}
+burger();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      lg: {max: '1999.99px'},
+      md: {max: '991.99px'},
+      sm: {max: '767.99px'},
+      xs: {max: '479.99px'},
+    },
+    extend: {
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif']
+      },
+      backgroundColor: {
+        green:'#22C55E',
+        darkGreay:'#2A3342',
+        lightGreay:'#556987',
+        footerGray: '#8896AB',
+      },
+    },
   },
   plugins: [],
 }
