f5b9abddfb1d64f13c3aeae7a4bae59d419d7db8
Nino Sopoian <ninosopoian@gmail.com>
2024-01-16 19:45:44 +0400

    add responsive header, burgerbar menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,14 @@
 function burger() {
-    const burgerMenu = document.querySelector('#burger-menu')
+    const burgerMenu = document.querySelector('#burger-menu');
+    const navigation = document.querySelector('#navigation');
+    const logInButton = document.querySelector('#actions');
 
     burgerMenu.addEventListener('click', ()=> {
-        burgerMenu.classList.toggle('active')
-        
+        burgerMenu.classList.toggle('active');
+        navigation.classList.toggle('burger-active')
+        logInButton.classList.toggle('actions-active')
     })
      
 }
 burger();
+
